4dc33db75a2edff133dd8c3a40a18ab76cbd5361
Bernard Krehula <krehula.bernard456@gmail.com>
2026-02-05 14:38:32 +0100

    Navbar moved to navbar component

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,26 +1,10 @@
-import Btn from "../../components/btn";
 import "./index.css";
-import { FaPowerOff } from "react-icons/fa6";
+import NavBar from "./navbar";
 
 const Homepage = () => {
   return (
     <div className="homepage">
-      <nav className="navbar">
-        <div className="logo-content">
-          <img
-            className="logo"
-            src="https://cinema-100-nemanja-malesija.netlify.app/assets/logo-21d86a09.png"
-          />
-          <span>cinema 100</span>
-        </div>
-        <span className="welcome-message">Welcome back, Guest</span>
-        <div className="logout-option">
-          <span>Log out</span>
-          <Btn type='button'>
-            <FaPowerOff />
-          </Btn>
-        </div>
-      </nav>
+      <NavBar />
     </div>
   );
 };
---
+++ src/pages/homepage/navbar/index.tsx
@@ -0,0 +1,25 @@
+import "./index.css";
+import { FaPowerOff } from "react-icons/fa6";
+import Btn from "../../../components/btn";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo-content">
+        <img
+          className="logo"
+          src="https://cinema-100-nemanja-malesija.netlify.app/assets/logo-21d86a09.png"
+        />
+        <span>cinema 100</span>
+      </div>
+      <span className="welcome-message">Welcome back, Guest</span>
+      <div className="logout-option">
+        <span>Log out</span>
+        <Btn type="button">
+          <FaPowerOff />
+        </Btn>
+      </div>
+    </nav>
+  );
+};
+export default NavBar;
