08790be9e52bac88702c0f38df33861fd9bc4e5b
Bernard Krehula <krehula.bernard456@gmail.com>
2026-02-06 00:50:40 +0100

    Slider setup

--- src/components/slider/index.tsx
+++ src/components/slider/index.tsx
@@ -2,19 +2,63 @@ import "./index.css";
 import SliderImg from "./SliderImg";
 import { requestTrendingMovies } from "../../api/requestTrendingMovies";
 import { useQuery } from "@tanstack/react-query";
+import Btn from "../../ui/btn";
+import { MdKeyboardArrowLeft } from "react-icons/md";
+import { MdKeyboardArrowRight } from "react-icons/md";
+import { useState } from "react";
 
 const Slider = () => {
+  const [position, setPosition] = useState<number>(0);
   const { data: trendingMovies } = useQuery({
     queryKey: ["trending-movies"],
-    queryFn: () => requestTrendingMovies()
+    queryFn: () => requestTrendingMovies(),
   });
+
+  const slideOnClick = (side: string) => {
+    if (!trendingMovies) return;
+
+    const visibleItems = 4;
+    const itemWidth = 230;
+    const gap = 5;
+
+    const step = visibleItems * (itemWidth + gap);
+    const totalWidth = trendingMovies.length * (itemWidth + gap);
+    const visibleWidth = visibleItems * (itemWidth + gap);
+
+    const maxTranslate = totalWidth - visibleWidth;
+
+    if (side === "right") {
+      setPosition((prev) => (Math.abs(prev) >= maxTranslate ? 0 : prev - step));
+    }
+
+    if (side === "left") {
+      setPosition((prev) => (prev === 0 ? -maxTranslate : prev + step));
+    }
+  };
+
   return (
     <div className="slider">
-      {trendingMovies?.map((movie) => {
-        const { id, image } = movie;
+      <h2 className="slider-title">Currently trending</h2>
+      <div className="slider-content">
+        <Btn type="button" onClick={() => slideOnClick("left")}>
+          <MdKeyboardArrowLeft />
+        </Btn>
+        <div className="slider-box">
+          <div
+            className="slider-box-content"
+            style={{ transform: `translateX(${position}px)` }}
+          >
+            {trendingMovies?.map((movie) => {
+              const { id, image } = movie;
 
-        return <SliderImg key={id} src={image} />;
-      })}
+              return <SliderImg key={id} src={image} />;
+            })}
+          </div>
+        </div>
+        <Btn type="button" onClick={() => slideOnClick("right")}>
+          <MdKeyboardArrowRight />
+        </Btn>
+      </div>
     </div>
   );
 };
