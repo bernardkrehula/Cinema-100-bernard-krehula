077a84a6d905a862c1b3b3448462c85a6b07d3a8
Bernard Krehula <krehula.bernard456@gmail.com>
2026-02-15 12:35:30 +0100

    Get movie with tanstack query

--- src/api/requestSingleMovie.ts
+++ src/api/requestSingleMovie.ts
@@ -1,11 +1,12 @@
 import supabase from "#/config/supabaseClientVite";
 
-export const requestSingleMovie = async (id: string) => {
+export const requestSingleMovie = async (id: string | undefined) => {
   try {
     const response = await supabase
     .from("movies")
     .select()
     .eq('id', id)
+    .single()
 
     return response.data;
   } catch (error) {
--- src/pages/singleMovie/index.tsx
+++ src/pages/singleMovie/index.tsx
@@ -1,17 +1,32 @@
-import { useParams } from 'react-router'
-import './index.css'
-import { useEffect } from 'react';
+import { useParams } from "react-router";
+import "./index.css";
+import { requestSingleMovie } from "#/api/requestSingleMovie";
+import { useQuery } from "@tanstack/react-query";
 
 const SingleMovie = () => {
-    const id = useParams();
-    console.log('radi singlemovie', id.movieID)
-    useEffect(() => {
+  const { movieID } = useParams();
+  const { data: movie } = useQuery({
+    queryKey: [`movie-${movieID}`],
+    queryFn: () => requestSingleMovie(movieID),
+  });
 
-    });
-    return(
-        <div className='single-movie'>
-
-        </div>
-    )
-}
+  if (!movie) return;
+  const {
+    id,
+    title,
+    imdbid,
+    rank,
+    genre,
+    description,
+    director,
+    writers,
+    image,
+    trailer,
+    thumbnail,
+    rating,
+    year
+  } = movie;
+  console.log(id);
+  return <div className="single-movie"></div>;
+};
 export default SingleMovie;
