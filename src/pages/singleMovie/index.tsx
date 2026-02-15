30d2fc742d9083ef70977db90011ad1d6078ad31
Bernard Krehula <krehula.bernard456@gmail.com>
2026-02-15 13:10:43 +0100

    Setup movie html structure

--- src/pages/singleMovie/index.tsx
+++ src/pages/singleMovie/index.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router";
 import "./index.css";
 import { requestSingleMovie } from "#/api/requestSingleMovie";
 import { useQuery } from "@tanstack/react-query";
+import NavBar from "#/components/layouts/navbar";
 
 const SingleMovie = () => {
   const { movieID } = useParams();
@@ -14,7 +15,6 @@ const SingleMovie = () => {
   const {
     id,
     title,
-    imdbid,
     rank,
     genre,
     description,
@@ -22,11 +22,63 @@ const SingleMovie = () => {
     writers,
     image,
     trailer,
-    thumbnail,
     rating,
-    year
+    year,
   } = movie;
-  console.log(id);
-  return <div className="single-movie"></div>;
+  const convertedGenre = JSON.parse(genre);
+  const convertedDirector = JSON.parse(director);
+  const convertedWriters = JSON.parse(writers);
+
+  return (
+    <div className="single-movie" key={id}>
+      <NavBar />
+      <div className="single-movie-content">
+        <div>
+          <div>
+            <h1>{title}</h1>
+            <span>({year})</span>
+          </div>
+          <div>
+            <h2>Imdb rating</h2>
+            <span>{rating}</span>
+          </div>
+          <div>
+            <h2>Rank</h2>
+            <span>{rank}</span>
+          </div>
+        </div>
+        <div>
+          <img src={image} />
+          <iframe
+            width="560"
+            height="315"
+            src={trailer}
+            title="Movie trailer"
+            frameBorder="0"
+            allowFullScreen
+          />
+        </div>
+        <article>
+          <ul>
+            {convertedGenre.map((type: string, index: number) => (
+              <li key={index}>{type}</li>
+            ))}
+          </ul>
+          <p>{description}</p>
+          <dl>
+            <dt>Director</dt>
+            <dd>{convertedDirector}</dd>
+            <hr />
+            <dt>Writers</dt>
+            <ul>
+              {convertedWriters.map((type: string, index: number) => (
+                <li key={index}>{type}</li>
+              ))}
+            </ul>
+          </dl>
+        </article>
+      </div>
+    </div>
+  );
 };
 export default SingleMovie;
