67907c7942079720102d74c7c84edc515823df1a
Bernard Krehula <krehula.bernard456@gmail.com>
2026-02-22 20:56:08 +0100

    Setup calculateMovieRange

--- src/pages/movieList/index.tsx
+++ src/pages/movieList/index.tsx
@@ -1,20 +1,49 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import "./index.css";
 import { reuqestMovieList } from "#/api/requestMovieList";
 import { requestMoviesArrayLength } from "#/api/requestMoviesArrayLength";
+import SliderImg from "../homepage/slider/SliderImg";
 
 const MovieList = () => {
-  const moviesNum = useRef<number | null>(0);
-  const [movieListNumber, setMovieListNumber] = useState<number>(0);
+  const [listMovies, setListMovies] = useState([]);
+  const [moviePageNumbers, setMoviePageNumbers] = useState<number[]>([0]);
 
   const getMovieListNumber = async () => {
     const movieColumnlength = await requestMoviesArrayLength();
-    moviesNum.current = movieColumnlength;
+    const visibleMoviesNum = 12;
+
+    if (movieColumnlength) {
+      const movieNum = Math.ceil(movieColumnlength / visibleMoviesNum);
+      const list = [];
+
+      for (let i = 0; i <= movieNum; i++) {
+        list.push(i);
+      }
+      setMoviePageNumbers(list);
+    }
   };
   useEffect(() => {
-    /* reuqestMovieList(); */
     getMovieListNumber();
-  });
-  return <div className="movie-list"></div>;
+  }, []);
+
+  const calculateMovieRange = (e: React.MouseEvent<HTMLSpanElement>) => {
+    const moviePage = Number((e.target as HTMLSpanElement).textContent);
+    let indexRange = { from: 0, to: 11 };
+    if (moviePage != 1) {
+      indexRange.from += 12 * moviePage;
+      indexRange.to += 12 * moviePage;
+    } else {
+      indexRange.from = 0;
+      indexRange.to = 11;
+    }
+  };
+
+  return (
+    <div className="movie-list">
+      {moviePageNumbers.map((page) => {
+        return <span key={page} onClick={calculateMovieRange}>{page}</span>;
+      })}
+    </div>
+  );
 };
 export default MovieList;
