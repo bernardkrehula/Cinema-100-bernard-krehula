c584e22a116b95fb8470c42a44ab3f6bca082845
Bernard Krehula <krehula.bernard456@gmail.com>
2026-02-26 22:09:06 +0100

    Get movie keys by singleMovieRequest

--- src/components/layouts/movieToolbar/index.tsx
+++ src/components/layouts/movieToolbar/index.tsx
@@ -1,10 +1,28 @@
 import './index.css'
 import SearchBar from "#/components/ui/searchBar";
+import { useEffect, useState } from 'react';
+import { requestSingleMovie } from '#/api/requestSingleMovie';
 
 const MovieToolbar = () => {
+    const [options, setOptions] = useState<string[]>([]);
+
+    const handleOptions = async() => {
+        const movieSearch = await requestSingleMovie("top1");
+        setOptions(Object.keys(movieSearch));
+    }
+
+    useEffect(() => {
+        handleOptions();
+    },[])
+    
     return(
         <div className="movie-tool-bar">
             <SearchBar placeholder="Search" value=''/>
+            <select>
+                {options.map(opt => {
+                    return <option value={opt}>{opt}</option>
+                })}
+            </select>
         </div>
     )
 }
