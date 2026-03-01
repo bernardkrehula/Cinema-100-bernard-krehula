9bf73c227a49c816c36e864b0dfcea762f0ab920
Bernard Krehula <krehula.bernard456@gmail.com>
2026-03-01 20:05:09 +0100

    movie genre fiter designed

--- src/components/layouts/movieToolbar/index.tsx
+++ src/components/layouts/movieToolbar/index.tsx
@@ -1,29 +1,63 @@
-import './index.css'
+import "./index.css";
 import SearchBar from "#/components/ui/searchBar";
-import { useEffect, useState } from 'react';
-import { requestSingleMovie } from '#/api/requestSingleMovie';
+import { useEffect, useState } from "react";
+import { requestSingleMovie } from "#/api/requestSingleMovie";
+import Btn from "#/components/ui/btn";
+import { IoBookmark, IoBookmarkOutline } from "react-icons/io5";
+import { FaHouse } from "react-icons/fa6";
+import { IoMdArrowDropdown } from "react-icons/io";
 
 const MovieToolbar = () => {
-    const [options, setOptions] = useState<string[]>([]);
+  const [options, setOptions] = useState<string[]>([]);
+  const [savedMovies, setSavedMovies] = useState<boolean>(false);
+  const [selectHomepage, setSelectHomepage] = useState<boolean>(false);
 
-    const handleOptions = async() => {
-        const movieSearch = await requestSingleMovie("top1");
-        setOptions(Object.keys(movieSearch));
-    }
+  const handleOptions = async () => {
+    const movieSearch = await requestSingleMovie("top1");
+    setOptions(Object.keys(movieSearch));
+  };
 
-    useEffect(() => {
-        handleOptions();
-    },[])
-    
-    return(
-        <div className="movie-tool-bar">
-            <SearchBar placeholder="Search" value=''/>
-            <select>
-                {options.map(opt => {
-                    return <option value={opt}>{opt}</option>
-                })}
-            </select>
+  useEffect(() => {
+    handleOptions();
+  }, []);
+
+  const handleSavedMovies = () => {
+    setSavedMovies(true);
+    setSelectHomepage(false);
+  };
+  const handleHomepageSelect = () => {
+    setSelectHomepage(true);
+    setSavedMovies(false);
+  };
+
+  return (
+    <div className="movie-tool-bar">
+      <SearchBar placeholder="Search" value="" />
+      <label className="movie-options-parent">
+        <select id="movie-genre-filter" className="movie-options">
+          {options.map((opt) => {
+            return <option value={opt}>{opt}</option>;
+          })}
+        </select>
+        <div className="movie-options-arrow">
+          <IoMdArrowDropdown />
         </div>
-    )
-}
+      </label>
+      <Btn
+        type="button"
+        onClick={handleSavedMovies}
+        variation="secondary-small"
+      >
+        {savedMovies ? (
+          <IoBookmark className="tool-bar-bookmark" />
+        ) : (
+          <IoBookmarkOutline className="tool-bar-bookmark" />
+        )}
+      </Btn>
+      <Btn type="button" onClick={handleHomepageSelect}>
+        <FaHouse className="home" fill={selectHomepage ? "white" : "none"} />
+      </Btn>
+    </div>
+  );
+};
 export default MovieToolbar;
