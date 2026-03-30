0018a446a0bae8f733bcadecf350475e7d85b7c3
Bernard Krehula <krehula.bernard456@gmail.com>
2026-03-30 23:49:49 +0200

    feat: add new routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Homepage from "./pages/homepage";
 import Login from "./pages/login";
 import SingleMovie from "./pages/singleMovie";
+import LoginForm from "./components/auth/LoginForm";
+import SignUpForm from "./components/auth/SignUpForm";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +22,14 @@ const router = createBrowserRouter([
     path: "movie/:movieID",
     element: <SingleMovie />,
   },
+  {
+    path: '/login-form',
+    element: <LoginForm />
+  },
+  {
+    path: '/sign-up-form',
+    element: <SignUpForm />
+  }
 ]);
 
 export default router;
