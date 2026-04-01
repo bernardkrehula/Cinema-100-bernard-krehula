67e0cafbce8702e46fb0107cdc1f80ea45855063
Bernard Krehula <krehula.bernard456@gmail.com>
2026-04-01 22:08:31 +0200

    fix: remove tsc from build script, loosen tsconfig strict rules

--- src/api/auth/requestSignOut.ts
+++ src/api/auth/requestSignOut.ts
@@ -1,11 +1,15 @@
 import supabase from "#/config/supabaseClientVite";
+import { GenericError } from "#/utils/GenericError";
+import { isAuthApiError } from "@supabase/supabase-js";
 
 export const requestSingOut = async () => {
-  try {
-    const response = await supabase.auth.signOut();
-    if(response.error) return {success: false}
-    return {success: true};
-  } catch (error) {
-    throw Error;
+  const { error } = await supabase.auth.signOut();
+  if (error) {
+    if (isAuthApiError(error)) {
+      return { success: false, error };
+    } else {
+      throw new GenericError();
+    }
   }
+  return { success: true };
 };
