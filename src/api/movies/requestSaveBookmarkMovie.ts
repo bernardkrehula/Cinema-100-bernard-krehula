935c44bb965b632522e91ec43d8b237a0c565a1f
Bernard Krehula <krehula.bernard456@gmail.com>
2026-04-14 21:04:52 +0200

    feat: book movies on click with requestSaveBookmarkMovies function

---
+++ src/api/movies/requestSaveBookmarkMovie.ts
@@ -0,0 +1,26 @@
+import supabase from "#/config/supabaseClientVite";
+import type { MovieIconType } from "#/types/movie.types.ts/MovieIconType";
+import { GenericError } from "#/utils/GenericError";
+import { isAuthApiError } from "@supabase/supabase-js";
+
+export const requestSaveBookmarkMovie = async (movie: MovieIconType) => {
+  const {
+    data: {
+      session: {
+        user: { id: user_id },
+      },
+    },
+  } = await supabase.auth.getSession();
+  
+  const { data, error } = await supabase
+    .from("bookmarks")
+    .insert([{ user_id, ...movie}]);
+  if (error) {
+    if (isAuthApiError(error)) {
+      return { success: false, error };
+    } else {
+      throw new GenericError();
+    }
+  }
+  return { data, error };
+};
