ea21930b6a4ffb44205d53c9f544d8988045f68b
Bernard Krehula <krehula.bernard456@gmail.com>
2026-04-17 21:20:38 +0200

    feat: call requestSyncMoviesWithBookmarks on useEffect inside <Homepage> component

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -2,9 +2,22 @@ import NavBar from "#/components/layouts/navbar";
 import MovieList from "#/components/layouts/MovieList";
 import "./index.css";
 import Slider from "./slider";
-
+import { useEffect, useState } from "react";
+import { requestSyncMoviesWithBookmarks } from "#/api/movies/requestSyncMoviesWithBookmarks";
 
 const Homepage = () => {
+  const [isLoading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleSync = async() => {
+      await requestSyncMoviesWithBookmarks();
+      setLoading(true);
+    }
+    handleSync();
+     }, [])
+
+  if(!isLoading) return null
+
   return (
     <div className="homepage">
       <NavBar />
