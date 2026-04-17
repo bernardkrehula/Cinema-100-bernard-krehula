880c78ff0e6f2a00fa7e4705877f271d6c2735c8
Bernard Krehula <krehula.bernard456@gmail.com>
2026-04-17 21:43:07 +0200

    feat: hadnle saveMovie conditionally based on movie props

--- src/components/ui/MovieImg/index.tsx
+++ src/components/ui/MovieImg/index.tsx
@@ -1,32 +1,46 @@
-import { useState } from "react";
 import "./index.css";
 import { IoBookmarkOutline } from "react-icons/io5";
 import Btn from "#/components/ui/btn";
 import { IoBookmark } from "react-icons/io5";
 import { IoSearchOutline } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
+import { requestSaveBookmarkMovie } from "#/api/movies/requestSaveBookmarkMovie";
+import { requestRemoveBookmarkMovie } from "#/api/movies/requestRemoveBookmarkMovie";
+import { useBookMovie } from "#/hooks/useBookMovie";
 
-const MovieImg = ({ src, id }: { src: string, id?: string }) => {
-  const [savedMovie, setSavedMovie] = useState<boolean>(false);
+const MovieImg = ({ movie, table }) => {
+  const { id, image: src, isSaved } = movie;
   const navigate = useNavigate();
+  const { mutate: saveMovie } = useBookMovie(id, table);
 
-  const handleSaveMovie = () => setSavedMovie((prev) => !prev);
-
+  const handleSaveMovie = () => {
+    const handler = () => {
+      if(isSaved) return requestRemoveBookmarkMovie(id) 
+      else if(!isSaved) return requestSaveBookmarkMovie(movie);
+    };
+    saveMovie(handler);
+  };
   const openMovie = () => navigate(`/movie/${id}`);
 
   return (
     <article className="movie-img">
       <div className="movie-img-overlay">
-        <Btn type="button" onClick={handleSaveMovie} variation="secondary-small">
-          {savedMovie ? (
+        <Btn
+          type="button"
+          onClick={handleSaveMovie}
+          variation="secondary-small"
+        >
+          {isSaved ? (
             <IoBookmark className="checked-bookmark" />
           ) : (
             <IoBookmarkOutline className="unChecked-bookmark" />
           )}
         </Btn>
-        {id && <a onClick={openMovie}>
-          See more <IoSearchOutline />
-        </a>}
+        {id && (
+          <a onClick={openMovie}>
+            See more <IoSearchOutline />
+          </a>
+        )}
       </div>
       <img src={src} />
     </article>
