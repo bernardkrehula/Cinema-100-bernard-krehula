731c0c3a9a0ef7d7458247cdb517894ba6349d33
Bernard Krehula <krehula.bernard456@gmail.com>
2026-04-17 22:15:42 +0200

    feat: implement useMutation in useBookMovie hook

---
+++ src/hooks/useBookMovie.tsx
@@ -0,0 +1,45 @@
+import type { MoviesResponse } from "#/types/movie.types.ts/MoviesResponseType";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+export const useBookMovie = (id: string, table: string) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (handler: () => void) => handler(),
+
+    onMutate: async () => {
+      await queryClient.cancelQueries({ queryKey: [table] });
+          
+      const previousMovies = queryClient.getQueriesData({ queryKey: [table], exact: false });
+      queryClient.setQueriesData({queryKey: [table], exact: false}, (old: MoviesResponse) => {
+        return {
+          ...old,
+          data: old.data.map((movie) =>
+            movie.id === id
+              ? {
+                  ...movie,
+                  isSaved: !movie.isSaved,
+                }
+              : movie,
+          ),
+        };
+      });
+
+      return previousMovies;
+    },
+    onSuccess: () => {
+      queryClient.setQueriesData({queryKey: [table], exact: false}, (old: MoviesResponse) => {
+        return {
+          ...old,
+          data: [],
+        };
+      });
+      queryClient.invalidateQueries();
+    },
+    onError: (_error, _isSaved, context: unknown) => {
+      if (context) {
+        queryClient.setQueryData([table], context);
+      }
+    },
+  });
+};
