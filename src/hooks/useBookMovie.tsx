0098df3bd34fb2386e1ad4bdedcce7078c9db270
Bernard Krehula <krehula.bernard456@gmail.com>
2026-04-19 14:25:10 +0200

    Update useBookMovie.tsx

--- src/hooks/useBookMovie.tsx
+++ src/hooks/useBookMovie.tsx
@@ -9,31 +9,39 @@ export const useBookMovie = (id: string, table: string) => {
 
     onMutate: async () => {
       await queryClient.cancelQueries({ queryKey: [table] });
-          
-      const previousMovies = queryClient.getQueriesData({ queryKey: [table], exact: false });
-      queryClient.setQueriesData({queryKey: [table], exact: false}, (old: MoviesResponse) => {
-        return {
-          ...old,
-          data: old.data.map((movie) =>
-            movie.id === id
-              ? {
-                  ...movie,
-                  isSaved: !movie.isSaved,
-                }
-              : movie,
-          ),
-        };
-      });
 
+      const previousMovies = queryClient.getQueriesData({
+        queryKey: [table], exact: false
+      });
+      console.log(table)
+      queryClient.setQueriesData(
+        { queryKey: [table], exact: false },
+        (old: MoviesResponse) => {
+          return {
+            ...old,
+            data: old.data.map((movie) =>
+              movie.id === id
+                ? {
+                    ...movie,
+                    isSaved: !movie.isSaved,
+                  }
+                : movie,
+            ),
+          };
+        },
+      );
       return previousMovies;
     },
     onSuccess: () => {
-      queryClient.setQueriesData({queryKey: [table], exact: false}, (old: MoviesResponse) => {
-        return {
-          ...old,
-          data: [],
-        };
-      });
+      queryClient.setQueriesData(
+        { queryKey: [table], exact: false },
+        (old: MoviesResponse) => {
+          return {
+            ...old,
+            data: [],
+          };
+        },
+      );
       queryClient.invalidateQueries();
     },
     onError: (_error, _isSaved, context: unknown) => {
