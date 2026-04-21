80d80d031d955ad9f0344243d637c6a0f965af90
Bernard Krehula <krehula.bernard456@gmail.com>
2026-04-21 22:14:09 +0200

    reafctor: split filter logic with if statments for bookmarks and movie table

--- src/api/movies/requestFilteredMovies.ts
+++ src/api/movies/requestFilteredMovies.ts
@@ -12,12 +12,34 @@ export const requestFilteredMovies = async ({
       .select("*", { count: "exact" })
       .range(range.from, range.to);
 
+    const moviesId = (await query).data.map(({ id }) => id);
+
+    if ((await query).data[0]?.user_id) {
+      query = supabase
+        .from("movies")
+        .select("*", { count: "exact" })
+        .in("id", moviesId);
+    }
+   
     if (filters.genre) {
-      query = query.or(`genre.cs.["${filters.genre}"]`);
+      if ((await query).data[0]?.user_id) {
+        query = supabase
+          .from("movies")
+          .select("*", { count: "exact" })
+          .in("id", moviesId)
+          .or(`genre.cs.["${filters.genre}"]`);
+      } else query = query.or(`genre.cs.["${filters.genre}"]`);
     }
     if (filters.search) {
-      query = query.ilike("title", `%${filters.search}%`);
+      if ((await query).data[0]?.user_id) {
+        query = supabase
+          .from("movies")
+          .select("*", { count: "exact" })
+          .in("id", moviesId)
+          .ilike("title", `%${filters.search}%`);
+      } else query = query.ilike("title", `%${filters.search}%`);
     }
+  
     return query;
   } catch (error) {
     throw Error;
