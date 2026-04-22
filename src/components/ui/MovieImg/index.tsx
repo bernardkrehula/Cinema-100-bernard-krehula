c581398cbfe32271d6a9757e0f4b8061487d9632
Bernard Krehula <krehula.bernard456@gmail.com>
2026-04-22 17:24:28 +0200

    refactor: change props on request functions and custom hook

--- src/components/ui/MovieImg/index.tsx
+++ src/components/ui/MovieImg/index.tsx
@@ -8,22 +8,29 @@ import { requestSaveBookmarkMovie } from "#/api/movies/requestSaveBookmarkMovie"
 import { requestRemoveBookmarkMovie } from "#/api/movies/requestRemoveBookmarkMovie";
 import { useBookMovie } from "#/hooks/useBookMovie";
 import type { MovieIconType } from "#/types/movie.types.ts/MovieIconType";
+import { useState } from "react";
+import { useIsMovieSaved } from "#/hooks/useIsMovieSaved";
 
-const MovieImg = ({ movie, table }: {movie: MovieIconType, table?: string}) => {
-  const { id, image: src, isSaved } = movie;
-
+const MovieImg = ({
+  movie,
+  table,
+}: {
+  movie: MovieIconType;
+  table?: string;
+}) => {
+  const { id, image: src } = movie;
+  const { mutate: saveMovie } = useBookMovie(table);
+  const { isSaved } = useIsMovieSaved(id);
   const navigate = useNavigate();
-  const { mutate: saveMovie } = useBookMovie(id, table);
 
   const handleSaveMovie = () => {
     const handler = () => {
-      if(isSaved) return requestRemoveBookmarkMovie(id) 
-      else if(!isSaved) return requestSaveBookmarkMovie(movie);
+      if (isSaved) return requestRemoveBookmarkMovie(id);
+      else if (!isSaved) return requestSaveBookmarkMovie(id);
     };
     saveMovie(handler);
   };
   const openMovie = () => navigate(`/movie/${id}`);
-
   return (
     <article className="movie-img">
       <div className="movie-img-overlay">
