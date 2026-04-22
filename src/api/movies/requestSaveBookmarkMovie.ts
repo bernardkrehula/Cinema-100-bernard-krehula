f9e73b423de488fe3fd5adc0ac0376e6153d111d
Bernard Krehula <krehula.bernard456@gmail.com>
2026-04-22 20:08:20 +0200

    fix: add check to prevent duplicate bookmark if movie exist

--- src/api/movies/requestSaveBookmarkMovie.ts
+++ src/api/movies/requestSaveBookmarkMovie.ts
@@ -2,7 +2,7 @@ import supabase from "#/config/supabaseClientVite";
 import { GenericError } from "#/utils/GenericError";
 import { isAuthApiError } from "@supabase/supabase-js";
 
-export const requestSaveBookmarkMovie = async (id: string) => {
+export const requestSaveBookmarkMovie = async (movie_id: string) => {
   const {
     data: {
       session: {
@@ -11,9 +11,16 @@ export const requestSaveBookmarkMovie = async (id: string) => {
     },
   } = await supabase.auth.getSession();
 
+  let query = await supabase
+    .from("bookmarks")
+    .select("movie_id, user_id")
+    .match({ movie_id: movie_id, user_id: user_id });
+
+  if (query.data.length != 0) return;
+
   const { data, error } = await supabase
     .from("bookmarks")
-    .insert([{ id, user_id }]);
+    .insert([{ id: crypto.randomUUID(), movie_id, user_id }]);
 
   if (error) {
     if (isAuthApiError(error)) {
