153a0ac12475f79fd76b57741f85059387919b60
Bernard Krehula <krehula.bernard456@gmail.com>
2026-04-22 17:25:00 +0200

    remove: remove optimistic update from useBookMovie custom hook

--- src/hooks/useBookMovie.tsx
+++ src/hooks/useBookMovie.tsx
@@ -1,37 +1,12 @@
 import type { MoviesResponse } from "#/types/movie.types.ts/MoviesResponseType";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-export const useBookMovie = (id: string, table: string) => {
+export const useBookMovie = (table: string) => {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: async (handler: () => void) => handler(),
 
-    onMutate: async () => {
-      await queryClient.cancelQueries({ queryKey: [table] });
-
-      const previousMovies = queryClient.getQueriesData({
-        queryKey: [table], exact: false
-      });
-      console.log(table)
-      queryClient.setQueriesData(
-        { queryKey: [table], exact: false },
-        (old: MoviesResponse) => {
-          return {
-            ...old,
-            data: old.data.map((movie) =>
-              movie.id === id
-                ? {
-                    ...movie,
-                    isSaved: !movie.isSaved,
-                  }
-                : movie,
-            ),
-          };
-        },
-      );
-      return previousMovies;
-    },
     onSuccess: () => {
       queryClient.setQueriesData(
         { queryKey: [table], exact: false },
